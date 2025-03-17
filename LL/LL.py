1ee49c15b9d6c1b6e883d6a06648bf3102d86d56
Tommaso Capoccia <tommy.cap3006@gmail.com>
2025-03-17 21:43:22 +0100

    added the pop method

--- LL/LL.py
+++ LL/LL.py
@@ -13,12 +13,13 @@ class LinkedList:
       self.tail = new_node
       self.length = 1
 
+   #print_list method
    def print_list(self):
       temp = self.head  # temp è uguale alla head (è un nodo, quindi ha un valore e un next)
       while temp is not None:  # finché temp non è nullo
          print(temp.value)  # stampa il valore di temp
          temp = temp.next  # temp diventa il prossimo nodo
-
+   #append method
    def append(self, value):
       new_node = Node(value)
       if self.head is None:
@@ -29,13 +30,34 @@ class LinkedList:
          self.tail = new_node
       self.length += 1
       return True
+   #pop method
+   def pop(self):
+      if self.length == 0:
+         return None
+      temp = self.head
+      pre = self.head
+      while (temp.next):
+         pre = temp
+         temp = temp.next
+      self.tail = pre
+      self.tail.next = None
+      self.length -= 1
+      if self.length == 0:
+         self.head = None
+         self.tail = None
+      #for test purpose
+      return temp.value
 
 
 #---------------------MAIN----------------------#
 
 # Testare la lista collegata
-my_linked_list = LinkedList(4)
-for i in range(10):
-   my_linked_list.append(i)
+my_linked_list = LinkedList(1)
+my_linked_list.append(2)
 
-my_linked_list.print_list() # Stampa correttamente tutti i valori
+# (2) Items - Returns 2 Node
+print(my_linked_list.pop())
+# (1) Items - Returns 1 Node
+print(my_linked_list.pop())
+# (0) Items - Returns None
+print(my_linked_list.pop())
