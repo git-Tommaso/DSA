f72b4f8d92a1df07b345eecd8635f2b69346bfa6
Tommaso Capoccia <tommy.cap3006@gmail.com>
2025-03-20 22:09:56 +0100

    added the append, pop and prepend methods

--- DLL/DLL.py
+++ DLL/DLL.py
@@ -13,3 +13,40 @@ class DoublyLinkedList:
          print(temp.value)
          temp = temp.next
       
+   def append(self, value):
+      new_node = Node(value)
+      if self.head is None:
+         self.head = new_node
+         self.tail = new_node
+      else:
+         self.tail.next = new_node
+         new_node.prev = self.tail
+         self.tail = new_node
+      self.length += 1
+      return True
+   
+   def pop(self):
+      if self.length == 0:
+         return None
+      temp = self.tail
+      if self.length == 1:
+         self.head = None
+         self.tail = None
+      else:
+         self.tail = temp.prev
+         self.tail.next = None
+         temp.prev = None
+      self.length -= 1
+      return temp
+   
+   def prepend(self, value):
+      new_node = Node(value)
+      if self.length == 0:
+         self.head = new_node
+         self.tail = new_node
+      else:
+         new_node.next = self.head
+         self.head.prev = new_node
+         self.head = new_node
+      self.length += 1
+      return True
--- DLL/main.py
+++ DLL/main.py
@@ -1,4 +1,8 @@
 from DLL import DoublyLinkedList
 
 my_doubly_linked_list = DoublyLinkedList(7)
+my_doubly_linked_list.append(14)
+
+my_doubly_linked_list.prepend(0)
+
 my_doubly_linked_list.print_list()
