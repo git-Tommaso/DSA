1c039d8340a6756b4072d012790ffbd635970c5c
Tommaso Capoccia <tommy.cap3006@gmail.com>
2025-03-21 23:34:46 +0100

    in the dll folder added the pop_first, get,set insert and remove methods, and implemented them in the main

--- DLL/main.py
+++ DLL/main.py
@@ -1,8 +1,15 @@
 from DLL import DoublyLinkedList
 
-my_doubly_linked_list = DoublyLinkedList(7)
-my_doubly_linked_list.append(14)
+my_doubly_linked_list = DoublyLinkedList(1)
 
+my_doubly_linked_list.append(2)
+my_doubly_linked_list.append(3)
 my_doubly_linked_list.prepend(0)
 
+my_doubly_linked_list.pop_first()
+
+print(my_doubly_linked_list.get(0))
+print(my_doubly_linked_list.get(1))
+
+print(my_doubly_linked_list.set_value(4,90))
 my_doubly_linked_list.print_list()
