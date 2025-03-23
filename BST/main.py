42b342966234ac7c9b3571cb2f00209e2d9eb943
Tommaso Capoccia <tommy.cap3006@gmail.com>
2025-03-23 13:30:53 +0100

    added the insert and contains methods in BST

--- BST/main.py
+++ BST/main.py
@@ -2,4 +2,17 @@ from BST import BinarySearchTree
 
 my_tree = BinarySearchTree()
 
-print(my_tree.root) # None 
+my_tree.insert(47)
+my_tree.insert(21)
+my_tree.insert(76)
+my_tree.insert(18)
+my_tree.insert(27)
+my_tree.insert(52)
+my_tree.insert(82)
+
+print(my_tree.root.value)
+print(my_tree.root.left.value)
+print(my_tree.root.right.value)
+
+print(my_tree.contains(27))
+print(my_tree.contains(17))
