1512056fcf9ed82d923bbb26ac062b9724492f38
Tommaso Capoccia <tommy.cap3006@gmail.com>
2025-03-26 21:56:32 +0100

    added the keys methods and implemented it in ghe main

--- HT/main.py
+++ HT/main.py
@@ -4,9 +4,12 @@ my_hash_table = HashTable() #size = 7
 
 my_hash_table.set_item('bolts', 1400)
 my_hash_table.set_item('washers', 50)
+my_hash_table.set_item('lumber', 70)
 
 print(my_hash_table.get_item('bolts'))
 print(my_hash_table.get_item('washers'))
 print(my_hash_table.get_item('lumber'))
 
 my_hash_table.print_table()
+print()
+print(my_hash_table.keys())
