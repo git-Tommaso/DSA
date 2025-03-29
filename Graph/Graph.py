a9c3994805e09b8cf2b48e0d0da5130edcf507e4
Tommaso Capoccia <tommy.cap3006@gmail.com>
2025-03-29 17:54:42 +0100

    added the add and remove edges methods

--- Graph/Graph.py
+++ Graph/Graph.py
@@ -13,3 +13,20 @@ class Graph:
          self.adj_list[vertex] = []
          return True
       return False
+   
+   def add_edge(self, v1, v2):
+      if v1 in self.adj_list.keys() and v2 in self.adj_list.keys():
+         self.adj_list[v1].append(v2)
+         self.adj_list[v2].append(v1)
+         return True
+      return False
+   
+   def remove_edge(self, v1, v2):
+      if v1 in self.adj_list.keys() and v2 in self.adj_list.keys():
+         try:
+            self.adj_list[v1].remove(v2)
+            self.adj_list[v2].remove(v1)
+         except ValueError:
+            pass
+         return True
+      return False
--- Graph/main.py
+++ Graph/main.py
@@ -1,6 +1,16 @@
 from Graph import Graph
 
 my_graph = Graph()
-my_graph.add_vertex("A")
+
+my_graph.add_vertex('A')
+my_graph.add_vertex('B')
+my_graph.add_vertex('C')
+my_graph.add_vertex('D')
+
+my_graph.add_edge('A', 'B')
+my_graph.add_edge('B', 'C')
+my_graph.add_edge('C', 'A')
+
+my_graph.remove_edge('A', 'D')
 
 my_graph.print_graph()
